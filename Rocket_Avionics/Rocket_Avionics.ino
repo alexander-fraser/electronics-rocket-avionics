98e42f69a2472e77c889dbbc9cf64ac666118fe0
Alexander Fraser <alexfraser4@gmail.com>
2021-12-19 15:33:57 -0500

    Remove end-time of program.

--- Rocket_Avionics/Rocket_Avionics.ino
+++ Rocket_Avionics/Rocket_Avionics.ino
@@ -9,9 +9,9 @@
 //
 // The MPU6050 and BMP280 are connected via I2C. 
 // The MicroSD card reader is connected using SPI.
-// The datalogger runs at its maximum sample rate (currently 37/sec). 
-// The data is stored to the MicroSD card as a CSV file. 
-// The datalogger starts at power-up and runs for 10 minutes.
+// The datalogger runs at its maximum sample rate (currently 25/sec). 
+// The data is stored to the MicroSD card as a CSV file.
+// The datalogger starts at power-up. 
 
 
 // Libraries:
@@ -44,7 +44,7 @@ void setup() {
  
   // Initialize the MicroSD card.
   Serial.print("Initializing SD card...");
-  if (!SD.begin(10)) {
+  if (!SD.begin(pinChipSelect)) {
     Serial.println("Failed to find the SD card.");
     digitalWrite(pinErrorLED, HIGH);
     while (1);
@@ -70,9 +70,9 @@ void setup() {
     digitalWrite(pinErrorLED, HIGH);
     while (1);
   }
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_260_HZ);
   Serial.println("MPU6050 initialized.");
 
   // Create the field headings within the data file on the MicroSD card.
@@ -97,43 +97,39 @@ void setup() {
 
 
 void loop() {
-  
-  // Run the datalogger for 10 minutes from startup.
-  while (millis() < 600000) {
+       
+  // Request the data from the MPU6050.
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
 
-    // Request the data from the MPU6050.
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
-  
-    // Open the file. If the file is available, write to it. If not, report an error.
-    File dataFile = SD.open("DATALOG.TXT", FILE_WRITE);
-  
-    if (dataFile) {
-      dataFile.print(micros());
-      dataFile.print(",");
-      dataFile.print(bmp.readTemperature(),2);
-      dataFile.print(",");
-      dataFile.print(bmp.readPressure(),2);
-      dataFile.print(",");
-      dataFile.print(bmp.readAltitude(1013.25),2);
-      dataFile.print(",");
-      dataFile.print(a.acceleration.x,3);
-      dataFile.print(",");
-      dataFile.print(a.acceleration.y,3);
-      dataFile.print(",");
-      dataFile.print(a.acceleration.z,3);
-      dataFile.print(",");
-      dataFile.print(g.gyro.x,4);
-      dataFile.print(",");
-      dataFile.print(g.gyro.y,4);
-      dataFile.print(",");
-      dataFile.println(g.gyro.z,4);
-      dataFile.close();
-    }
-    else {
-      Serial.println("Error opening the log file.");
-      digitalWrite(pinErrorLED, HIGH);
-    }
-    
+  // Open the file. If the file is available, write to it. If not, report an error.
+  File dataFile = SD.open("DATALOG.TXT", FILE_WRITE);
+
+  if (dataFile) {
+    dataFile.print(micros());
+    dataFile.print(",");
+    dataFile.print(bmp.readTemperature(),2);
+    dataFile.print(",");
+    dataFile.print(bmp.readPressure(),2);
+    dataFile.print(",");
+    dataFile.print(bmp.readAltitude(1013.25),2);
+    dataFile.print(",");
+    dataFile.print(a.acceleration.x,3);
+    dataFile.print(",");
+    dataFile.print(a.acceleration.y,3);
+    dataFile.print(",");
+    dataFile.print(a.acceleration.z,3);
+    dataFile.print(",");
+    dataFile.print(g.gyro.x,4);
+    dataFile.print(",");
+    dataFile.print(g.gyro.y,4);
+    dataFile.print(",");
+    dataFile.println(g.gyro.z,4);
+    dataFile.close();
   }
+  else {
+    Serial.println("Error opening the log file.");
+    digitalWrite(pinErrorLED, HIGH);
+  }
+    
 }
